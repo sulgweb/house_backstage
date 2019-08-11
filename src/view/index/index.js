73aadf5495417d9b45dedf7bb2600fb01606dda6
lyff1006 <821760648@qq.com>
2019-08-11 16:48:18 +0800

    引入antd，并更改为按需加载

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import { Layout, Menu, Icon ,Button} from 'antd';
+import React from 'react'
 
-function index() {
-  return (
-    <div>index</div>
-  );
+const { Header, Sider, Content } = Layout;
+export default class Index extends React.Component{
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+  render(){
+      return(
+        <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+            <Button type="primary">Primary</Button>
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280,
+            }}
+          >
+            Content 
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
 }
-
-export default index;
