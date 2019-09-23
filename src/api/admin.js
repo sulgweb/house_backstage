18727e82a9d57ec296b27e63e2f47176b2c27c02
lyff <821760648@qq.com>
2019-09-23 22:13:36 +0800

    添加admin api

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 let baseUrl = "http://localhost:7001"
 
-class admin {
-    async adminLogin(){
-
+class Admin {
+    async adminLogin(data){
+        let res = await axios.post(`${baseUrl}/admin/adminLogin`,data).then(r=>{
+            return r
+        })
+        return res.data
+    }
+    async addAdmin(data){
+        let res = await axios.post(`${baseUrl}/admin/addAdmin`,data).then(r=>{
+            return r
+        })
+        return res.data
     }
 }
+
+let admin = new Admin()
+export {admin}
